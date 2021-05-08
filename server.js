41b192b735358ee678adadf40d2e2278417d09e6
udit2910 <uditdj29@gmail.com>
2021-05-08 13:03:20 +0530

    prepare fro deploy

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 3600;
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
-const { getVaccineData } = require('./service/service');
 const cron = require('node-cron');
+const path = require('path');
+const { getVaccineData } = require('./service/service');
 
 
 app.listen(port, () => console.log('listening on port %s', port));
@@ -17,6 +18,15 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
   console.log('time :', Date.now());
 });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+  }
+  
 
 cron.schedule('*/5 * * * * *', () => {
     getVaccineData();
